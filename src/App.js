75da20837ed47ed3d65bfd77b1f941d0c6681d6d
groove.kim <groove.kim@gmail.com>
2020-07-10 12:40:29 +0900

    20200710_학습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+
+function Food({name, picture, rating}) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}/>
+    </div>
+  );
+}
+
+const foodLike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image: "https://ucloud.lgcns.com/VmCube/Media/template/TPL_UCLD/images/portal/header_logo.png",
+    rating: 5
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image: "https://ucloud.lgcns.com/VmCube/Media/template/TPL_UCLD/images/portal/header_logo.png",
+    rating: 4.8
+  },
+  {
+    id: 3,
+    name: "Bibimbap",
+    image: "https://ucloud.lgcns.com/VmCube/Media/template/TPL_UCLD/images/portal/header_logo.png",
+    rating: 4.6
+  },
+  {
+    id: 4,
+    name: "Doncasu",
+    image: "https://ucloud.lgcns.com/VmCube/Media/template/TPL_UCLD/images/portal/header_logo.png",
+    rating: 4.8
+  },
+  {
+    id: 5,
+    name: "Kimbap",
+    image: "https://ucloud.lgcns.com/VmCube/Media/template/TPL_UCLD/images/portal/header_logo.png",
+    rating: 4.3
+  }
+];
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodLike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />))}
     </div>
+    
   );
 }
 
